cf3f7a7e70b5317a93cbe53566873a95a9e984fa
Aryan Sangave <123397860+aryan36a@users.noreply.github.com>
2025-08-07 12:08:34 +0530

    minor changes

--- Test_Files/temp.py
+++
@@ -1,7 +0,0 @@
-nums1=[4,1,2]
-nums2=[1,3,4,2]
-for i in range(len(nums1)):
-    for j in range(len(nums2)):
-        if nums1[i]==nums2[j]:
-            for x in range(len(nums2)):
-                if nums2[x]>
